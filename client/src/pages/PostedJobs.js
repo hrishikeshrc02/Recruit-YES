62af53552d8bb0b8a6f7bf8a61f22f7d2dda85c5
Hrishikesh Roy Choudhury <hrishirc2018@gmail.com>
2024-05-22 01:36:33 +0530

    changed posted jobs

--- client/src/pages/PostedJobs.js
+++ client/src/pages/PostedJobs.js
@@ -1,26 +1,22 @@
 import React, { useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Table, Modal } from "antd";
 import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
   EditOutlined,
   OrderedListOutlined,
 } from "@ant-design/icons";
 import moment from "moment";
 import { Link, useHistory } from "react-router-dom";
+
 function PostedJobs() {
-  const alljobs = useSelector((state) => state.jobsReducer).jobs;
-  const allusers = useSelector((state) => state.usersReducer).users;
+  const alljobs = useSelector((state) => state.jobsReducer.jobs);
+  const allusers = useSelector((state) => state.usersReducer.users);
   const userid = JSON.parse(localStorage.getItem("user"))._id;
-  const userPostedJobs = alljobs.filter((job) => job.postedBy == userid);
+  const userPostedJobs = alljobs.filter((job) => job.postedBy === userid);
   const history = useHistory();
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [selectedJob, setSelectedJob] = useState();
+  const [selectedJob, setSelectedJob] = useState(null);
 
   const columns = [
     {
@@ -41,45 +37,37 @@ function PostedJobs() {
     },
     {
       title: "Actions",
-      render: (text, data) => {
-        return (
-          <div className="flex">
-            <EditOutlined
+      render: (text, data) => (
+        <div className="flex">
+          <EditOutlined
             className='mr-2'
-              style={{fontSize:20}}
-              onClick={() => {
-                history.push(`/editjob/${data.completeJobData._id}`);
-              }}
-            />
-            <OrderedListOutlined
-               style={{fontSize:20}}
-              onClick={() => {
-                
-                showModal(job);
-              }}
-            />
-          </div>
-        );
-      },
+            style={{ fontSize: 20 }}
+            onClick={() => {
+              history.push(`/editjob/${data.completeJobData._id}`);
+            }}
+          />
+          <OrderedListOutlined
+            style={{ fontSize: 20 }}
+            onClick={() => {
+              showModal(data.completeJobData);
+            }}
+          />
+        </div>
+      ),
     },
   ];
 
-  const dataSource = [];
-
-  for (var job of userPostedJobs) {
-    var obj = {
-      title: job.title,
-      company: job.company,
-      postedOn: moment(job.createdAt).format("MMM DD yyyy"),
-      appliedCandidates: job.appliedCandidates.length,
-      completeJobData: job,
-    };
-    dataSource.push(obj);
-  }
+  const dataSource = userPostedJobs.map((job) => ({
+    title: job.title,
+    company: job.company,
+    postedOn: moment(job.createdAt).format("MMM DD yyyy"),
+    appliedCandidates: job.appliedCandidates.length,
+    completeJobData: job,
+  }));
 
   const showModal = (job) => {
-    setIsModalVisible(true);
     setSelectedJob(job);
+    setIsModalVisible(true);
   };
 
   const handleOk = () => {
@@ -90,59 +78,48 @@ function PostedJobs() {
     setIsModalVisible(false);
   };
 
- function CandidatesList(){
-  const candidatesColumns = [
-    {
-      title: "Candidate Id",
-      dataIndex: "candidateId",
-      render : (text ,data)=>{
-       return <Link to={`/users/${data.candidateId}`}>{data.candidateId}</Link>
-      }
-    },
-    {
-      title: "Full Name",
-      dataIndex: "fullName",
-    },
-    { title: "Applied Date", dataIndex: "appliedDate" },
-  ];
+  function CandidatesList() {
+    const candidatesColumns = [
+      {
+        title: "Candidate Id",
+        dataIndex: "candidateId",
+        render: (text, data) => (
+          <Link to={`/users/${data.candidateId}`}>{data.candidateId}</Link>
+        ),
+      },
+      {
+        title: "Full Name",
+        dataIndex: "fullName",
+      },
+      { title: "Applied Date", dataIndex: "appliedDate" },
+    ];
 
-  var candidatesDatasource = [];
+    const candidatesDatasource = selectedJob ? selectedJob.appliedCandidates.map((candidate) => {
+      const user = allusers.find((user) => user._id === candidate.userid);
 
-  for (var candidate of selectedJob.appliedCandidates) {
-    var user = allusers.find((user) => user._id == candidate.userid);
+      return {
+        candidateId: user._id,
+        fullName: `${user.firstName} ${user.lastName}`,
+        appliedDate: moment(candidate.appliedDate).format("MMM DD yyyy"),
+      };
+    }) : [];
 
-    var obj = {
-      candidateId: user._id,
-      fullName: user.firstName + " " + user.lastName,
-      appliedDate: candidate.appliedDate,
-    };
-
-    candidatesDatasource.push(obj);
+    return <Table columns={candidatesColumns} dataSource={candidatesDatasource} />;
   }
 
-  return <Table
-  columns={candidatesColumns}
-  dataSource={candidatesDatasource}
-/>
- }
-
-  console.log(userPostedJobs);
   return (
     <div>
       <DefaultLayout>
         <h1>Posted Jobs</h1>
-
         <Table columns={columns} dataSource={dataSource} />
-
         <Modal
           title="Applied Candidates List"
           visible={isModalVisible}
-          closable={false}
           onOk={handleOk}
           onCancel={handleCancel}
           width={800}
         >
-          <CandidatesList/>
+          {selectedJob && <CandidatesList />}
         </Modal>
       </DefaultLayout>
     </div>
@@ -150,3 +127,4 @@ function PostedJobs() {
 }
 
 export default PostedJobs;
+
